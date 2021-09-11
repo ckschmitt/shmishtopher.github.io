8a6a330cdc8a94b9fd5203931f1c35ed1e07c5e9
Shmish <shmish90@gmail.com>
2021-09-11 12:20:45 -0400

    Fixed layout padding and added pinned & homework sections

--- script/repository.js
+++ script/repository.js
@@ -3,7 +3,7 @@
 // @legal     MIT     "(c) Shmish"
 
 
-fetch('https://api.github.com/users/shmishtopher/repos?sort=pushed&type=owner')
+fetch('https://api.github.com/users/shmishtopher/repos?sort=created&type=owner')
   .then(response => response.json())
   .then(repos => repos.slice(0, 6))
   .then(repos => {
@@ -21,3 +21,52 @@ fetch('https://api.github.com/users/shmishtopher/repos?sort=pushed&type=owner')
       document.querySelector('#repos').appendChild(clone)
     }
   })
+
+
+fetch('https://gh-pinned-repos-5l2i19um3.vercel.app/?username=shmishtopher')
+  .then(response => response.json())
+  .then(repos => {
+    for (const repo of repos) {
+      const teplate = document.querySelector('template')
+      const clone = document.importNode(teplate.content, true)
+
+      clone.querySelector('a').setAttribute('href', repo.link)
+      clone.querySelector('span').setAttribute('language', repo.language)
+
+      clone.querySelector('a').childNodes[0].nodeValue = repo.repo
+      clone.querySelector('span').innerText = repo.language
+      clone.querySelector('p').innerText = repo.description
+
+      document.querySelector('#pinned').appendChild(clone)
+    }
+  })
+
+
+const homework = Promise.all([
+  fetch('https://api.github.com/repos/shmishtopher/CS-253').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/CS-355').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/CS-354').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/CS-254').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/CS-153').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/CS-385').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/CS-483').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/CS-492').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/CS-463').then(x => x.json()),
+  fetch('https://api.github.com/repos/shmishtopher/Semantic-Web').then(x => x.json()),
+])
+
+homework.then(repos => {
+  for (const repo of repos) {
+    const teplate = document.querySelector('template')
+    const clone = document.importNode(teplate.content, true)
+
+    clone.querySelector('a').setAttribute('href', repo.html_url)
+    clone.querySelector('span').setAttribute('language', repo.language)
+
+    clone.querySelector('a').childNodes[0].nodeValue = repo.name
+    clone.querySelector('span').innerText = repo.language
+    clone.querySelector('p').innerText = repo.description
+
+    document.querySelector('#homework').appendChild(clone)
+  }
+})
